80f911473a35bd0fdd24ab3c4abbac68dde48e2b
Jonathan Silverman <jsilverman2@gmail.com>
2015-11-09 20:00:01 -0700

    make the warning system production ready. you will get it as soon as you open the IM.

--- injected.js
+++ injected.js
@@ -2,63 +2,71 @@ warning = function(warning) {
   // document.addEventListener("DOMNodeInserted", null);
   var messages = document.getElementsByClassName('messages show');
   var element = messages[0];
-  if(element) {
+  if(element && !(element.innerHTML.indexOf(warning) > -1)) {
     element.innerHTML += "<li><div class='message yours'>" + warning + "</div></li>";
     element.scrollTop = element.scrollHeight - element.clientHeight;
   }
   // do it in the messages screen too
   // for now just alert
   messages = document.getElementById("message_heading");
-  if(messages) {
+  if(messages && !(messages.innerHTML.indexOf(warning) > -1)) {
     messages.innerHTML += "<p>" + warning + "</p>";
   }
   // document.addEventListener("DOMNodeInserted", dom_inserted);
   // alert(warning);
 }
 
-dom_inserted = function(e) {
-  if(e.target)
-    var target = e.target.tagName.toLowerCase();
-  else
-    return false; // fail
-  // console.log("target: " + target);
-  if(target == "li" && target.className == "to_me")
-    check_for_bullshit();
+// dom_inserted = function(e) {
+//   if(e.target.tagName)
+//     var target = e.target.tagName.toLowerCase();
+//   else
+//     return false; // fail
+//   // console.log(e.target.className);
+//   // console.log("target: " + target);
+//   if((target == "li" && e.target.className == "to_me") || (e.target.className == "enter_to_send_checkbox"))
+//     check_for_bullshit();
+// }
+
+function check_msg(msg) {
+  console.log(msg);
+  msg = msg.toLowerCase();
+  if(msg.indexOf("skype") > -1) {
+    warning("Warning: The remote party mentioned Skype. In the past Skype has been used to hijack video stream for scam purposes. Only Skype with people you know and trust.");
+  } else if (msg.indexOf("text") > -1) {
+    warning("Warning: If the remote party is asking you to text them right now, doing so would reveal your phone number, which can help build a profile on your identity.");
+  } else if (msg.indexOf("name") > -1) {
+    warning("Warning: The remote party may have asked your name. It is probably unwise to reveal this information.");
+  }
 }
 
 
-check_for_bullshit = function() {
+function check_for_bullshit() {
+  // console.log("ran");
   var theirs = document.getElementsByClassName("theirs");
   var message_body = document.getElementsByClassName("message_body");
-  console.log(theirs);
+  // console.log(theirs);
   // scan IM box
   for(i = 0; i < theirs.length; i++) {
     msg = theirs[i].innerHTML;
-    console.log(msg);
-    if((msg.indexOf("skype") > -1) || (msg.indexOf("Skype") > -1)) {
-      warning("Warning: The remote party mentioned Skype. In the past Skype has been used to hijack video stream for scam purposes. Only Skype with people you know and trust.");
-    } else if ((msg.indexOf("text") > -1) || (msg.indexOf("Text") > -1)) {
-      warning("Warning: If the remote party is asking you to text them right now, doing so would reveal your phone number, which can help build a profile on your identity.");
-    }
+    check_msg(msg);
+
   }
-  // scan messages screen
-  for(i = 0; i < message_body.length; i++) {
-    msg = message_body[i].innerHTML;
-    console.log(msg);
-    if((msg.indexOf("skype") > -1) || (msg.indexOf("Skype") > -1)) {
-      warning("Warning: The remote party mentioned Skype. In the past Skype has been used to hijack video stream for scam purposes. Only Skype with people you know and trust.");
-    } else if ((msg.indexOf("text") > -1) || (msg.indexOf("Text") > -1)) {
-      warning("Warning: If the remote party is asking you to text them right now, doing so would reveal your phone number, which can help build a profile on your identity.");
+  // scan messages screen if IM wasn't scanned...
+  if(theirs.length == 0) {
+    for(i = 0; i < message_body.length; i++) {
+      msg = message_body[i].innerHTML;
+      check_msg(msg);
     }
   }
 }
 
 
 window.onload = function() {
-  console.log("I loaded!!!!");
+  // console.log("I loaded!!!!");
   check_for_bullshit();
 
-  document.addEventListener("DOMNodeInserted", dom_inserted);
+  setInterval(check_for_bullshit, 5000);
 
-  // document.getElementsByClassName('maximize')[0].addEventListener('click', dom_inserted);
+  // document.addEventListener("DOMNodeInserted", dom_inserted);
+
 }
